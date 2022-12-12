6cfbe62e5dbfc13c65971c6351909fc65084d67f
gotlr98 <gotlr98@naver.com>
2022-12-12 21:01:00 +0900

    feat : add sentence

--- Test.swiftpm/MyApp.swift
+++ Test.swiftpm/MyApp.swift
@@ -2,9 +2,11 @@ import SwiftUI
 
 @main
 struct MyApp: App {
+    @StateObject private var modelData = ModelData()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
         }
     }
 }
