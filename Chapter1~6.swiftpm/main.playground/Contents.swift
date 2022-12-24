ce4dd86ba64ef5b8bedc9a9e5213ed6b94be2431
gotlr98 <gotlr98@naver.com>
2022-12-24 13:28:09 +0900

    feat : reorganization file and add content function type

--- Chapter1~6.swiftpm/main.playground/Contents.swift
+++ Chapter1~6.swiftpm/main.playground/Contents.swift
@@ -1,3 +1,42 @@
+import UIKit
+
+//var greeting = "Hello, playground"
+//
+//struct BasicInformation{
+//    let name: String
+//    var age: Int
+//}
+//
+//var yagomInfo: BasicInformation = BasicInformation(name: "yagom", age: 99)
+//
+//class Person{
+//    var height: Float = 0.0
+//    var weight: Float = 0.0
+//}
+//
+//let yagom: Person = Person()
+//yagom.height = 182.5
+//yagom.weight = 78.5
+//
+//print(yagomInfo)
+//dump(yagomInfo)
+//
+//let name: String = "Haesik"
+//print("My name is \(name)")
+
+// var [변수명]: [데이터 타입] = [값]
+var name: String = "yagom"
+var age: Int = 100
+var job = "iOS Programmer"
+var height = 181.5
+
+age = 99
+
+// let -> 상수 let [상수명]: [데이터 타입] = [값]
+
+let name: String = "yagom"
+name = "야곰"
+
 import Foundation
 
 
--- Chapter7~.swiftpm/main.playground/Contents.swift
+++ Chapter7~.swiftpm/main.playground/Contents.swift
@@ -27,6 +27,82 @@ func sayHello(from myName:String, to name:String) -> String{
     return "Hello \(name)! I'm \(myName)"
 }
 
-print(sayHello(from: "Haesik", to: "Yoorim"))
+print(sayHello(from: "Haesik", to: "Son"))
 
 
+func sayHello(_ name: String, _ times: Int) -> String{
+    var result: String = ""
+    
+    for _ in 0..<times{
+        result += "Hello \(name)!" + " "
+    }
+    
+    return result
+}
+
+print(sayHello("Chope", 2))
+
+func sayHello(_ name: String, times: Int = 3) -> String{
+    var result: String = ""
+    
+    for _ in 0..<times{
+        result += "Hello \(name)!" + " "
+    }
+    return result
+}
+
+print(sayHello("Hana"))
+
+// 가변 매개변수와 입출력 매개변수
+
+func sayHelloToFriends(me: String, friends names: String...) -> String { // 가변 매개변수는 0개이상 가능하다
+    var result: String = ""
+    
+    for friend in names {
+        result += "Hello \(friend)!" + " "
+    }
+    
+    result += "I'm " + me + "!"
+    return result
+}
+
+print(sayHelloToFriends(me: "Haesik", friends: "Taehyi", "Yongjin", "Yoorim"))
+print(sayHelloToFriends(me: "Haesik"))
+
+// 반환이 없는 함수
+
+func sayHelloWorld() -> Void { // void는 생략가능
+    print("Hello, world")
+}
+
+// 함수 타입의 사용
+
+typealias CalculateTwoInts = (Int, Int) -> Int
+
+func addTwoInts(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+
+func multiplyTwoInts(_ a: Int, _ b: Int) -> Int{
+    return a * b
+}
+
+var mathFunction: CalculateTwoInts = addTwoInts // == var mathFunction: (Int, Int) -> Int = addTwoInts
+
+print(mathFunction(2,5))
+
+mathFunction = multiplyTwoInts
+print(mathFunction(2,5))
+
+
+func printMathResult(_ mathFunction: CalculateTwoInts, _ a: Int, _ b:Int){
+    print("Result: \(mathFunction(a,b))")
+}
+
+printMathResult(addTwoInts, 3, 5)
+
+func chooseMathFunction(_ toAdd: Bool) -> CalculateTwoInts {
+    return toAdd ? addTwoInts : multiplyTwoInts
+}
+
+printMathResult(chooseMathFunction(true), 3, 5)
