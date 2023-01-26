03da8cbacdd479a3f69d7800dded3623e04232c7
gotlr98 <gotlr98@naver.com>
2023-01-26 11:58:23 +0900

    feat : add chapter 24~ file

---
+++ Chapter 24~30.swiftpm/main.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+// Chapter 24 타입 중첩
+
+/*
+ 타입 내부에 새로운 타입을 선언해준 것을 중첩 타입이라고 부른다.
+ */
+
+class Person{
+    enum Job{
+        case jobless, programmer, student
+    }
+    
+    var job: Job = .jobless
+}
+
+class Student: Person{
+    enum School{
+        case elementary, middle, high
+    }
+    
+    var school: School
+    
+    init(school: School){
+        self.school = school
+        super.init()
+        self.job = .student
+    }
+}
+
+let personJob: Person.Job = .jobless
+let studentJob: Student.Job = .student
+
+let student: Student = Student(school: .middle)
