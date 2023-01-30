84caf755442f2ef746b166afc584e19d36f226f5
gotlr98 <gotlr98@naver.com>
2023-01-30 15:05:22 +0900

    feat : chapter 24 done & add chapter 25

--- Chapter 24~30.swiftpm/main.playground/Contents.swift
+++ Chapter 24~30.swiftpm/main.playground/Contents.swift
@@ -34,4 +34,69 @@ let personJob: Person.Job = .jobless
 let studentJob: Student.Job = .student
 
 let student: Student = Student(school: .middle)
-let sik: Student = Student(school: .university)
+
+
+// 같은 이름의 중첩 데이터 타입 구현
+
+struct Sports{
+    enum GameType{
+        case football, basketball
+    }
+    
+    var gameType: GameType
+    
+    struct GameInfo{
+        var time: Int
+        var player: Int
+    }
+    
+    var gameInfo: GameInfo{
+        switch self.gameType{
+        case .basketball:
+            return GameInfo(time: 40, player: 5)
+        case .football:
+            return GameInfo(time: 90, player: 11)
+        }
+    }
+}
+
+struct ESports{
+    enum GameType{
+        case online, offline
+    }
+    
+    var gameType: GameType
+    
+    struct GameInfo{
+        var location: String
+        var pakage: String
+    }
+    
+    var gameInfo: GameInfo{
+        switch self.gameType{
+        case .online:
+            return GameInfo(location: "www.liveonline.co.kr", pakage: "LOL")
+        case .offline:
+            return GameInfo(location: "제주", pakage: "SA")
+        }
+    }
+}
+
+var basketball: Sports = Sports(gameType: .basketball)
+print(basketball.gameInfo)
+
+let someGameType: Sports.GameType = .football
+let anotherGameType: ESports.GameType = .online
+
+//let errorIfYouWantIt: Sports.GameType = .online // 같은 이름이더라도 다른 구조체에 선언되어있기 때문에 사용불가
+
+
+
+// Chapter 25 패턴
+/*
+ 패턴은 '단독 또는 복합 값의 구조를 나타내는 것'이고, 패턴 매칭은 '코드에서 어떤 패턴의 형태를 찾아내는 행위'
+ 쉽게 말해서 이러이러한 것을 표현하고 싶다면 이러이러한 패턴을 통해 표현하면 된다 라는 뜻이다.
+ 
+ 1. 값을 해체(추출)하거나 무시하는 패턴 - 와일드카드 패턴, 식별자 패턴, 값 바인딩 패턴, 튜플 패턴
+ 2. 패턴 매칭을 위한 패턴 - 열거형 케이스 패턴, 옵셔널 패턴, 표현 패턴, 타입캐스팅 패턴
+ */
