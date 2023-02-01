d48b3113b03b8d73415271307c7d641f6f304cad
gotlr98 <gotlr98@naver.com>
2023-02-01 13:03:29 +0900

    feat : chapter 26 done

--- Chapter 24~30.swiftpm/main.playground/Contents.swift
+++ Chapter 24~30.swiftpm/main.playground/Contents.swift
@@ -356,3 +356,143 @@ switch star{
 case isNamed("Jung"): print("He is Jung")
 default: print("Another person")
 }
+
+
+
+// Chapter 26 where 절
+/*
+ 1. 패턴과 결합하여 조건 추가
+ 2. 타입에 대한 제약 추가
+ 
+ 특정 패턴에 Bool 타입 조건을 지정하거나 어떤 타입의 특정 프로토콜 준수 조건을 추가하는 등의 기능
+ */
+
+let tuples: [(Int, Int)] = [(1, 2), (1, -1), (1, 0), (0, 2)]
+
+for tuple in tuples{
+    switch tuple{
+    case let (x, y) where x == y: print("x == y")
+    case let (x, y) where x == -y: print("x == -y")
+    case let (x, y) where x > y: print("x > y")
+    case let (1, _): print("x == 1")
+    case let (_, 2): print("y == 2")
+    default: print("\(tuple.0), \(tuple.1)")
+    }
+}
+
+var repeatCount: Int = 0
+
+for tuple in tuples{
+    switch tuple{
+    case let (x, y) where x == y && repeatCount > 2: print("x == y")
+    case let (x, y) where repeatCount < 2: print("\(x), \(y)")
+    default: print("Nothing")
+    }
+    
+    repeatCount += 1
+}
+
+let firstValue: Int = 50
+let secondValue: Int = 30
+
+switch firstValue + secondValue{
+case let total where total > 100: print("total > 100")
+case let total where total < 0: print("wrong value")
+case let total where total == 0: print("zero")
+case let total: print(total)
+}
+
+
+for case let number? in arrayOfOptionalInts where number > 2{
+    print("Found a \(number)")
+}
+
+let anyValue: Any = "ABC"
+
+switch anyValue{
+case let value where value is Int: print("value is Int")
+case let value where value is String: print("value is String")
+case let value where value is Double: print("value is Double")
+default: print("Unknown type")
+}
+
+var things: [Any] = [Any]()
+
+things.append(0)
+things.append(0.0)
+things.append(42)
+things.append(3.14159)
+things.append("hello")
+things.append((3.0, 5.0))
+things.append({(name: String) -> String in "Hello, \(name)"})
+
+for thing in things{
+    switch thing{
+    case 0 as Int:
+        print("zero as an Int")
+    case 0 as Double:
+        print("zero as a Double")
+    case let someInt as Int:
+        print("an integer value of \(someInt)")
+    case let someDouble as Double where someDouble > 0:
+        print("a positive double value of \(someDouble)")
+    case is Double:
+        print("some other double value that I don't want to print")
+    case let someString as String:
+        print("a string value of \"\(someString)\"")
+    case let (x, y) as (Double, Double):
+        print("an (x, y) point at \(x), \(y)")
+    case let stringConverter as (String) -> String:
+        print(stringConverter("Michael"))
+    default:
+        print("something else")
+    }
+}
+
+
+var point2: (Int, Int) = (1, 2)
+
+switch point2{
+case (0, 0): print("원점")
+case (-2...2, -2...2) where point2.0 != 1: print("\(point2.0), \(point2.1)")
+default: print("point (\(point2.0), \(point2.1))")
+}
+
+
+// where 절을 활용한 타입 매개변수와 연관 타입의 타입 제약 추가
+
+func doubled<T>(integerValue: T) -> T where T: BinaryInteger{
+    return integerValue * 2
+}
+
+//func doubled<T: BinaryInteger>(integerValue: T) -> T{ // 위와 같은 표현
+//    return integerValue * 2
+//}
+
+func compareTwoSequences<S1, S2>(a: S1, b: S2) where S1: Sequence, S1.Element: Equatable, S2: Sequence, S2.Element: Equatable{
+    
+}
+
+protocol Container{
+    associatedtype ItemType where ItemType: BinaryInteger
+    var count: Int{get}
+    mutating func append(_ item: ItemType)
+    subscript(i: Int) -> ItemType{get}
+}
+
+
+// where 절을 활용한 제네릭 타입의 연관 타입 제약 추가
+
+protocol Talkable{}
+protocol CallToAll{
+    func callToAll()
+}
+
+struct PersonW: Talkable{}
+struct Animal{}
+
+extension Array: CallToAll where Element: Talkable{
+    func callToAll(){}
+}
+
+
