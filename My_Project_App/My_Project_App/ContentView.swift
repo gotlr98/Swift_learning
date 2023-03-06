f7485ee14f606d65803a068118b0bd8014dba854
gotlr98 <gotlr98@naver.com>
2023-03-06 16:31:24 +0900

    feat : build frame

--- My_Project_App/My_Project_App/ContentView.swift
+++ My_Project_App/My_Project_App/ContentView.swift
@@ -6,20 +6,92 @@
 //
 
 import SwiftUI
+import WebKit
+
+struct MyWebView: UIViewRepresentable {
+   
+    
+    var urlToLoad: String
+    
+    //ui view 만들기
+    func makeUIView(context: Context) -> WKWebView {
+        
+        //unwrapping
+        guard let url = URL(string: self.urlToLoad) else {
+            return WKWebView()
+        }
+        //웹뷰 인스턴스 생성
+        let webView = WKWebView()
+        
+        //웹뷰를 로드한다
+        webView.load(URLRequest(url: url))
+        return webView
+    }
+    
+    //업데이트 ui view
+    func updateUIView(_ uiView: WKWebView, context: UIViewRepresentableContext<MyWebView>) {
+        
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Button(action: {
+        
+        NavigationView{
+            VStack {
                 
-            }, label: {
-                Rectangle()
-                    .frame(width:50, height: 50)
-                    .font(.system(size: 25))
-                    .foregroundColor(Color.black)
-            })
+                GeometryReader{ geo in
+                    
+                    NavigationLink(destination:{
+                        MyWebView(urlToLoad: "www.naver.com")
+                        
+                    }
+
+                    , label: {
+                        VStack{
+                            Rectangle()
+                                .foregroundColor(Color.gray)
+                                .frame(width: 130, height: 50)
+                                .cornerRadius(15)
+                                .overlay{
+                                    Text("Naver Login")
+                                        .foregroundColor(Color.white)
+                                        .fontWeight(.black)
+                                }
+                            
+                        }
+                        .position(x: geo.size.width / 2, y: geo.size.height / 1.7)
+                        
+                    })
+                
+                    NavigationLink(destination:{
+                        MyWebView(urlToLoad: "www.google.com")
+                    
+                    }
+                    , label: {
+                        VStack{
+                            
+                            Rectangle()
+                                .foregroundColor(Color.gray)
+                                .frame(width: 130, height: 50)
+                                .cornerRadius(15)
+                                .overlay{
+                                    Text("Google Login")
+                                        .foregroundColor(Color.white)
+                                        .fontWeight(.black)
+                                }
+                                
+                        }
+                        .position(x: geo.size.width / 2, y: geo.size.height / 1.5)
+ 
+                    })
+                }
+               
+            }
+            .padding()
         }
-        .padding()
+        
+        
     }
 }
 
