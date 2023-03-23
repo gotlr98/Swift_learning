c5246141693ad70086a88b743396623d6faaa673
gotlr98 <gotlr98@naver.com>
2023-03-23 14:04:51 +0900

    feat : add memo function & main view

--- Memo_Tutorial/Memo_Tutorial/Memo_TutorialApp.swift
+++ Memo_Tutorial/Memo_Tutorial/Memo_TutorialApp.swift
@@ -9,12 +9,16 @@ import SwiftUI
 
 @main
 struct Memo_TutorialApp: App {
+    
+    // 모든 뷰에서 공유가능
+    @StateObject var store = MemoStore()
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
             MainListView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(store)
         }
     }
 }
