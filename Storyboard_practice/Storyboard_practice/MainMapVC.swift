1db53ed3da992286a540b591b896758dfa5beb7d
gotlr98 <gotlr98@naver.com>
2024-10-03 17:00:43 +0900

    practice storyboard

---
+++ Storyboard_practice/Storyboard_practice/MainMapVC.swift
@@ -0,0 +1,21 @@
+//
+//  MainMapVC.swift
+//  Storyboard_practice
+//
+//  Created by HaeSik Jang on 10/3/24.
+//
+
+import Foundation
+import UIKit
+
+class MainMapVC: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("hello")
+    }
+    @IBAction func Click_Btn1(_ sender: UIButton) {
+        
+    }
+}
---
+++ Storyboard_practice/Storyboard_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Storyboard_practice
+//
+//  Created by HaeSik Jang on 10/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
